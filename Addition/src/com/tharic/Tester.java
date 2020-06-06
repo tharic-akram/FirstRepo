9c3d8808929578022fb88fd8fec71e4a0f3e5784
ELCOT <ELCOT@Lenovo>
2020-06-06 15:03:43 +0530

    adding class to test in sonarqube

---
+++ Addition/src/com/tharic/Tester.java
@@ -0,0 +1,24 @@
+package com.tharic;
+
+public class Tester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(Tester.add(45,67));
+		
+		System.out.println(Tester.sub(67,34));
+
+	}
+
+	private static int sub(int i, int j) {
+		// TODO Auto-generated method stub
+		return i+j;
+	}
+
+	private static int add(int i, int j) {
+		// TODO Auto-generated method stub
+		return i-j;
+	}
+	
+
+}
